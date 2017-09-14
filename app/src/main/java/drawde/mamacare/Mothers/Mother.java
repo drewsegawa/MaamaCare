446ead93a8188c357f5d1cba5a2e70a566214cb2
unknown <Joseph.Basoga@BAJOSEPH-PC.ESP.local>
2017-09-15 00:40:21 +0300

    Commented on Mother.java

--- app/src/main/java/drawde/mamacare/Mothers/Mother.java
+++ app/src/main/java/drawde/mamacare/Mothers/Mother.java
@@ -1,13 +1,13 @@
 package drawde.mamacare.Mothers;
 
-/**
- * Created by emute on 14/09/2017.
- */
 
 public class Mother {
     String id, name;
 
     public Mother(String name, String id){
+    	/**
+    	 * Instantiating variables
+    	 */
         this.id = id;
         this.name = name;
     }
